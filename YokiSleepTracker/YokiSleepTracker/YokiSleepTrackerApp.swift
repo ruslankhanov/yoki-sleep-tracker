b827c3c6500d64e9f23a01bed8b73d49dd452259
ruslan.khanov <ruslan.khanov@onetwotrip.com>
2025-07-15 19:41:15 +0300

    init project

---
+++ YokiSleepTracker/YokiSleepTracker/YokiSleepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  YokiSleepTrackerApp.swift
+//  YokiSleepTracker
+//
+//  Created by Ruslan Khanov on 15.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct YokiSleepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YokiSleepTracker/YokiSleepTrackerTests/YokiSleepTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  YokiSleepTrackerTests.swift
+//  YokiSleepTrackerTests
+//
+//  Created by Ruslan Khanov on 15.07.2025.
+//
+
+import Testing
+@testable import YokiSleepTracker
+
+struct YokiSleepTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
